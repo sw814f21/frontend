1b3d408263ba24efe4cc9f55f0f30ba7f18fc4cb
Badgy <jonaskhansen@gmail.com>
2021-03-10 16:32:04 +0100

    setup i18n

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import './i18n/i18n'  // initializes i18n, required
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
---
+++ i18n/da.ts
@@ -0,0 +1,11 @@
+const daMessages = {
+    nav: {
+        search_title: 'Søg',
+        notifications_title: 'Notifikationer',
+        map_title: 'Kort',
+        favorite_title: 'Favoritter',
+        profile_title: 'Profil',
+    }
+}
+
+export default daMessages;
