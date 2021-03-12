8538bf2d0aa7b6708a285909b410939088f3994a
Badgy <jonaskhansen@gmail.com>
2021-03-12 14:32:32 +0100

    IT WOOOOOOOOOOOOOOOOOOOORKS

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
-import { StyleSheet, Dimensions } from 'react-native';
+import {StyleSheet, Dimensions, FlatList, Linking} from 'react-native';
 import MapView from 'react-native-maps';
 import { Marker } from "react-native-maps";
-import {Restaurant} from "../types";
+import {Restaurant, SmileyReport} from "../types";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { getRestaurants } from "../api/sample_api";
 import {Component} from "react";
+import {FontAwesome} from "@expo/vector-icons";
+import {
+    EliteSmiley,
+    Smiley,
+    SmileyHappy, SmileyNeutral,
+    SmileyOkay,
+    SmileySad,
+} from "../components/Smileys";
+import i18n from "../i18n/i18n";
+import {Link} from "@react-navigation/native";
 
 
 interface MapScreenProps {
@@ -30,6 +40,78 @@ interface Region {
 interface MapStyle {
     width: number,
     height: number
+}
+
+interface SmileyData {
+    smiley: any,
+    smileyText: string
+}
+
+function smileyFromKey(key: number) : SmileyData {
+    let smiley;
+    let smileyText;
+    let smileyProps = {
+        viewBox: '0 0 97.3333 97.3333',
+    }
+    let eliteSmileyProps = {
+        viewBox: '0 0 188 145.3333',
+    }
+    switch (key) {
+        case Smiley.Elite:
+            smiley = EliteSmiley(eliteSmileyProps);
+            smileyText = i18n.t('smileyText.elite')
+            break;
+        case Smiley.Bad:
+            smiley = SmileySad(smileyProps);
+            smileyText = i18n.t('smileyText.bad')
+            break;
+        case Smiley.Decent:
+            smiley = SmileyOkay(smileyProps);
+            smileyText = i18n.t('smileyText.decent')
+            break;
+        case Smiley.Good:
+            smiley = SmileyHappy(smileyProps);
+            smileyText = i18n.t('smileyText.good')
+            break;
+        case Smiley.Neutral:
+            smiley = SmileyNeutral(smileyProps);
+            smileyText = i18n.t('smileyText.neutral')
+            break;
+        default:
+            smiley = SmileyHappy(smileyProps);
+            smileyText = i18n.t('smileyText.good')
+            break;
+    }
+    return {
+        smiley: smiley,
+        smileyText: smileyText
+    };
+}
+
+function ReportItem({ report }: { report: SmileyReport}) {
+    let smiley: SmileyData = smileyFromKey(report.smiley)
+
+    return (
+        <View
+            style={styles.listItem}
+        >
+            <View style={styles.iconCol}>
+                {smiley.smiley}
+            </View>
+            <View style={styles.nameCol}>
+                <Text >{report.date}</Text>
+                <Text >{smiley.smileyText}</Text>
+            </View>
+            <View style={styles.iconCol}>
+                <FontAwesome
+                    name={"external-link"}
+                    color={"#236683"}
+                    size={40}
+                    onPress={() => Linking.openURL(report.url)}
+                />
+            </View>
+        </View>
+    )
 }
 
 
@@ -90,13 +172,34 @@ export default class MapScreen extends Component<MapScreenProps, MapScreenState>
 
     renderRestaurant() {
         if (this.state.restaurant) {
+            let smiley = smileyFromKey(this.state.restaurant.cur_smiley);
+
             return (
                 <View style={styles.container}>
                     {this.renderMap(styles.restaurantMap)}
                     <View
                         style={styles.restaurant}
+
                     >
-                        <Text>{this.state.restaurant.name}</Text>
+                        <View style={styles.listHeader}>
+                            <View style={styles.iconCol}>
+                            <FontAwesome name={'star-o'} color={"#236683"} size={40}/>
+                            </View>
+                            <View style={styles.nameCol}>
+                                <Text style={styles.title}>{this.state.restaurant.name}</Text>
+                                <Text >{this.state.restaurant.address}</Text>
+                                <Text >{this.state.restaurant.zip_code} {this.state.restaurant.city}</Text>
+                            </View>
+                            <View style={styles.iconCol}>
+                            {smiley.smiley}
+                            </View>
+                        </View>
+
+                        <FlatList
+                            data={this.state.restaurant.reports}
+                            renderItem={( {item} ) => <ReportItem report={item}/>}
+                            keyExtractor={(item, _) => item.date.toString()}
+                        />
 
                     </View>
                 </View>
@@ -132,4 +235,27 @@ const styles = StyleSheet.create({
         height: Dimensions.get('window').height * .6,
         backgroundColor: "white"
     },
+    title: {
+        fontSize: 20
+    },
+    listHeader: {
+        padding: 20,
+        marginHorizontal: 0,
+        flexDirection: 'row',
+        maxHeight: "30%"
+    },
+    listItem: {
+        flex: 1,
+        padding: 20,
+        flexDirection: "row",
+        alignContent: "center"
+    },
+    iconCol: {
+        width: Dimensions.get('window').width * .2,
+    },
+    nameCol: {
+        width: Dimensions.get('window').width * .5,
+    }
 });
+
+
