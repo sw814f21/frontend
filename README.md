0a0492f16c61a08a3d403f5512bf6b2c115dc37c
Jonas Andersen <jonas@anrsn.me>
2021-03-12 08:45:34 +0100

    Added search view (#16)
    * Added search view
    
    * Textinput themed
    
    * Render smileys
    
    * Smileys & stuff
    
    * Icons'n'stuff
    
    * Update readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # FindSmiley frontend
+
+
+## Setup guide
+### 0. Install node.js `15.10`  
+Use [nvm](https://github.com/nvm-sh/nvm) to install it
+```
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash 
+```
+***IMPORTANT***: Use node.js version 15.10
+### 1. Install expo
+```
+npm install --global expo-cli
+```
+### 2. Change to dir `frontend/`
+```bash
+cd frontend/
+```
+### 3. Install rest of the packages
+```
+npm install
+```
+### 4. Start the app in tunnel mode (For external possibilities)
+```
+expo start --host tunnel
+```
---
+++ screens/SearchScreen.tsx
@@ -0,0 +1,94 @@
+import { FontAwesome } from '@expo/vector-icons';
+import * as React from 'react';
+import { StyleSheet, FlatList, Image } from 'react-native';
+
+import { Text, View, TextInput } from '../components/Themed';
+import { getRestaurants } from "../api/sample_api";
+import { SmileyHappy, SmileyNeutral, SmileyOkay, SmileySad } from "../components/Smileys";
+import { Restaurant, Smiley } from "../types";
+import i18n from "../i18n/i18n";
+
+
+const DATA = getRestaurants()['restaurants'];
+
+function Item({ restaurant }: { restaurant: Restaurant }) {
+  let currSmiley;
+  switch (restaurant.cur_smiley) {
+    case Smiley.Bad:
+      currSmiley = SmileySad;
+      break;
+    case Smiley.Decent:
+      currSmiley = SmileyOkay;
+      break
+    case Smiley.Good:
+      currSmiley = SmileyHappy;
+      break;
+    case Smiley.Neutral:
+      currSmiley = SmileyNeutral;
+      break;
+    default:
+      currSmiley = SmileyHappy;
+      break;
+  }
+  return (
+    <View style={styles.listitem}>
+      <FontAwesome name='star-o' color='white' size={30} style={styles.icon} />
+      {currSmiley({ width: '10%' })}
+      <View>
+        <Text style={styles.title}>{restaurant.name}</Text>
+        <Text>{restaurant.address}</Text>
+      </View>
+      <View style={{ marginLeft: 'auto' }}>
+        <Text>6.1km</Text>
+      </View>
+    </View>
+  );
+}
+
+export default function SearchScreen() {
+  const [value, onChangeText] = React.useState("");
+  return (
+    <View style={styles.container}>
+      <View style={styles.search}>
+        <FontAwesome name='search' color='white' size={20} style={styles.icon} />
+        <TextInput placeholder={i18n.t('search.placeholder')} style={styles.flex} />
+      </View>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item restaurant={item} />}
+        keyExtractor={(item, _) => item.id.toString()}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  listitem: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+  },
+  icon: {
+    marginRight: 10,
+  },
+  flex: {
+    flex: 1,
+  },
+  search: {
+    flexDirection: 'row',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+});
