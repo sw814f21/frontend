072e256210b7a86b8a1d0d9b0d472ade656aab95
Badgy <jonaskhansen@gmail.com>
2021-03-15 09:35:15 +0100

    add restaurants to notifications

--- types.tsx
+++ types.tsx
@@ -69,8 +69,8 @@ export enum NotificationType {
 }
 
 export type Notification = {
-    restaurant_id: number;
     type: number;  // map to enum NotificationType
     date: string;
     new_data: any;
+    restaurant: Restaurant;
 }
