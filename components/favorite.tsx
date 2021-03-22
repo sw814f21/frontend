9962db711d80c22d8095435c3bcc409113b106a0
Badgy <jonaskhansen@gmail.com>
2021-03-22 12:35:35 +0100

    favorite star

--- components/favorite.tsx
+++ components/favorite.tsx
@@ -2,13 +2,35 @@ import {FontAwesome} from "@expo/vector-icons";
 import * as React from "react";
 import {storageAPI} from "../data/storage"
 import {Restaurant} from "../types";
+import {Component} from "react";
 
-export default function FavoriteStar({ restaurant }: { restaurant: Restaurant }) {
-    const name = restaurant.favorite ? 'star' : 'star-o';
-    return <FontAwesome
-        name={name}
-        color='#236683'
-        size={30}
-        onPress={() => storageAPI().toggleFavoriteStoredRestaurant(restaurant.id).then(() => {})}
-    />
+interface FavoriteStarState {
+    restaurant: Restaurant;
+}
+
+interface FavoriteStarProps {
+    restaurant: Restaurant;
+}
+
+export default class FavoriteStar extends Component<FavoriteStarProps, FavoriteStarState>{
+    constructor(props: any) {
+        super(props);
+        this.state = { restaurant: props.restaurant }
+        console.log(this.state.restaurant)
+    }
+
+    render() {
+        const name = this.state.restaurant.favorite ? 'star' : 'star-o';
+        return <FontAwesome
+            name={name}
+            color='#236683'
+            size={30}
+            onPress={() => {
+                storageAPI().toggleFavoriteStoredRestaurant(this.state.restaurant.id).then(() => {})
+                let copy = this.state.restaurant;
+                copy.favorite = !copy.favorite;
+                this.setState( {restaurant: copy })
+            }}
+        />
+    }
 }
