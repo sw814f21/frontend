52de9ffb14d5677b2f7296b5f56831a483da4075
Jonas Andersen <jonas@anrsn.me>
2021-03-23 11:58:18 +0100

    API implementation according to specification

--- api/api.ts
+++ api/api.ts
@@ -1,17 +1,49 @@
-import { Restaurant } from "../types";
+import { Restaurant, SparseRestaurant } from "../types";
 import { HTTPAPI } from "./http_api";
 import { SampleAPI } from "./sample_api";
 import Constants from 'expo-constants';
 
+/**
+ * The interface for getting data from the API.
+ * Makes it easier swap out the backend if necessary.
+ */
 export interface FindSmileyAPI {
   /**
-   * Fetches all the restaurants from the database
+   * Fetches sparse restaurants from the database
    */
-  getRestaurants(): Promise<Restaurant[]>;
+  getSparseRestaurants(): Promise<SparseRestaurant[]>;
+
   /**
    * Fetches a single restaurant from the database
    */
   getRestaurant(id: number): Promise<Restaurant>;
+
+  /**
+   * Searches for restaurants with a given name.
+   * @param name the name to search for
+   */
+  searchRestaurantByName(name: string): Promise<Restaurant[]>;
+
+  /**
+   * Searches for restaurants in a given rectangular area.
+   * @param northEast North-east point of the area to search in
+   * @param southwest South-west point of the area to search in
+   */
+  searchRestaurantByLocation(northEast: number, southwest: number): Promise<Restaurant[]>;
+
+  /**
+   * Subscribes the user to notifications on a given restaurant.
+   * @param token The token to use on the server to send the user a push notification.
+   * @param restaurantid The ID of the restaurant for which the user wants push notification.
+   */
+  subscribeToPushNotifications(token: string, restaurantid: number): Promise<void>;
+
+  /**
+   * Unsubscribes the user from notifications of a given restaurant.
+   * @param token The token which the user has used to subscribe to push notifications.
+   * @param restaurantid The ID of the restaurant for which the user wants to unsubscribe from notifications from.
+   */
+  unsubscribeFromPushNotification(token: string, restaurantid: number): Promise<void>;
 }
 
 const httpapi = new HTTPAPI();
--- api/http_api.ts
+++ api/http_api.ts
@@ -1,8 +1,11 @@
-import { Restaurant } from "../types";
+import { Restaurant, SparseRestaurant } from "../types";
 import { FindSmileyAPI } from "./api";
 import _axios from "axios";
 import Constants from 'expo-constants';
 
+/**
+ * Provides the actual HTTP API for communicating with the server.
+ */
 export class HTTPAPI implements FindSmileyAPI {
 
   private axios = _axios.create({
@@ -22,10 +25,70 @@ export class HTTPAPI implements FindSmileyAPI {
     });
   }
 
-  getRestaurants(): Promise<Restaurant[]> {
-    return new Promise<Restaurant[]>((resolve, reject) => {
-      this.axios.get('/restaurant/').then(res => {
+  getSparseRestaurants(): Promise<SparseRestaurant[]> {
+    return new Promise<SparseRestaurant[]>((resolve, reject) => {
+      this.axios.get('/restaurant').then(res => {
         resolve(res.data);
+      }).catch(res => {
+        reject(res);
+      })
+    });
+  }
+
+  searchRestaurantByName(searchName: string): Promise<Restaurant[]> {
+    return new Promise<Restaurant[]>((resolve, reject) => {
+      const parameters = {
+        params: {
+          name: searchName
+        }
+      };
+      this.axios.get('/restaurant/search', parameters).then(res => {
+        resolve(res.data);
+      }).catch(res => {
+        reject(res);
+      })
+    });
+  }
+
+  searchRestaurantByLocation(searchNorthEast: number, searchSouthWest: number): Promise<Restaurant[]> {
+
+    return new Promise<Restaurant[]>((resolve, reject) => {
+      const parameters = {
+        params: {
+          northEast: searchNorthEast,
+          southWest: searchSouthWest
+        }
+      };
+      this.axios.get('/restaurant/search', parameters).then(res => {
+        resolve(res.data);
+      }).catch(res => {
+        reject(res);
+      })
+    });
+  }
+
+  subscribeToPushNotifications(notificationToken: string, subRestaurantid: number): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const data = {
+        token: notificationToken,
+        restaurant: subRestaurantid
+      }
+      this.axios.post('/subscribe', data).then(_ => {
+        resolve();
+      }).catch(res => {
+        reject(res);
+      })
+    });
+  }
+
+  unsubscribeFromPushNotification(notificationToken: string, subRestaurantid: number): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const data = {
+        token: notificationToken,
+        restaurant: subRestaurantid
+      }
+      this.axios.post('/unsubscribe', data).then(_ => {
+        resolve();
       }).catch(res => {
         reject(res);
       })
