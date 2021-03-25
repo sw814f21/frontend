b3f4924e7b3422e82eae031ba5e61853617cc83f
Jonas Andersen <jonas@anrsn.me>
2021-03-25 09:45:49 +0100

    Remove error in notificationscreen

--- screens/NotificationScreen.tsx
+++ screens/NotificationScreen.tsx
@@ -45,7 +45,7 @@ export default class NotificationScreen extends Component<any, NotificationScree
     }
 
     componentDidMount() {
-        storageAPI().functiongetStoredNotifications().then(res => {
+        storageAPI().getStoredNotifications().then(res => {
             this.setState({
                 notifications: res
             })
